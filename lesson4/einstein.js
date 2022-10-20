2e710070e22eab51c50cb2c3b9805320ec7c8a12
“RomanKond” <kondrashovrs@gmail.com>
2022-10-20 21:35:44 +0300

    lesson4 v1.2

---
+++ lesson4/einstein.js
@@ -0,0 +1,15 @@
+'use strict';
+
+    const myWeight = 70;
+    const speedLight = 3e8;
+
+    const result = myWeight*(speedLight ** 2);
+
+
+    console.log(`==========================`);
+    console.log(`Старт работы einstein.js`);
+    console.log(`==========================`);
+    console.log(`Энергия равна: ${result}`);
+    console.log(`==========================`);
+    console.log(`Конец работы einstein.js`);
+    console.log(`==========================`);
--- lesson4/script.js
+++ lesson4/script.js
@@ -1,24 +1,18 @@
 'use strict';
 
-{
-    const itemName = 'Карандаш';
-    const itemCount = 3;
-    const itemCategory = 'Канцтовары';
-    const itemPriceRub = "300";
 
+    const itemName = prompt(`Введите наименование товара`);
+    const itemCount = parseInt(prompt(`Введите количество товара ${itemName}`)); //количество товара всегда целое (шт), поэтому parseInt
+    const itemCategory = prompt(`Введите категорию товара`);
+    const itemPriceRub = parseFloat(prompt(`Введите стоимость единицы товара`)); //стомость не обязательно целая, поэтому parseFloat
 
+    console.log(`==========================`);
+    console.log(`Старт работы script.js`);
+    console.log(`==========================`);
+    console.log(`Тип данных (количество товара): ${typeof itemCount}`);
+    console.log(`Тип данных (цена товара): ${typeof itemPriceRub}`);
 
-    console.log(itemName);
-}
-
-
-{
-    const itemName = 'Ручка';
-    const itemCount = 7;
-    const itemCategory = 'Канцтовары';
-    const itemPriceRub = "100";
-
-
-
-    console.log(itemName);
-}
+    console.log(`На складе ${itemCount} единиц(ы) товара "${itemName}" (из категории ${itemCategory}) на сумму ${itemCount*itemPriceRub} рублей`);
+    console.log(`==========================`);
+    console.log(`Конец работы script.js`);
+    console.log(`==========================`);
