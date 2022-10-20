5f05eda2da2777d5e0142bc594a3c47ce9bfa64d
“RomanKond” <kondrashovrs@gmail.com>
2022-10-20 21:11:01 +0300

    lesson4

---
+++ lesson4/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+    const itemName = 'Карандаш';
+    const itemCount = 3;
+    const itemCategory = 'Канцтовары';
+    const itemPriceRub = "300";
+
+
+
+    console.log(itemName);
+}
+
+
+{
+    const itemName = 'Ручка';
+    const itemCount = 7;
+    const itemCategory = 'Канцтовары';
+    const itemPriceRub = "100";
+
+
+
+    console.log(itemName);
+}
