bec8912917fb7cbdd9d1eaea4322bdb4de1b4b35
“RomanKond” <kondrashovrs@gmail.com>
2022-10-20 18:41:13 +0300

    Урок 4

--- script.js
+++ script.js
@@ -11,14 +11,3 @@
     console.log(itemName);
 }
 
-
-{
-    const itemName = 'Ручка';
-    const itemCount = 7;
-    const itemCategory = 'Канцтовары';
-    const itemPriceRub = "100";
-
-
-
-    console.log(itemName);
-}
