11a0253d7fca73bb4edccc7b8f8dbca5a124a4ec
“RomanKond” <kondrashovrs@gmail.com>
2022-10-24 00:27:42 +0300

    lesson9 game1 fixed

--- lesson9-adv/game01.js
+++ lesson9-adv/game01.js
@@ -5,18 +5,27 @@ const letsPlay = () => {
     let answer = -1;
 
     do{
-        answer = parseInt(prompt('Введите предполагаемое число'));
+        answer = prompt('Введите предполагаемое число');
 
-        if (answer == null || Number.isNaN(answer)) break;
+        //кнопка отмена
+        if (answer == null || answer === '') {
+            break;
+        }    
 
-        console.log(`Введено число: ${answer}`);
-        if (answer > myNumber){
-            console.log(`Загаданное число меньше`);
+        answer = parseInt(answer);
+
+        //неверное значение
+        if (Number.isNaN(answer)){
+            console.log('Введено неверное значение')
+        }else{
+            console.log(`Введено число: ${answer}`);
+            if (answer > myNumber){
+                console.log(`Загаданное число меньше`);
+            }
+            if (answer < myNumber){
+                console.log(`Загаданное число больше`);
+            }
         }
-        if (answer < myNumber){
-            console.log(`Загаданное число больше`);
-        }
-
 
         console.log(`---`);
     } while (answer !== myNumber);
